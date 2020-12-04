13572e20568f24fcfbac01336143ee6923a5f159
KeepSimple <nikita.iasnogorodskyi@startupcraft.io>
2020-12-04 17:48:04 +0200

    sticky header and footer

--- src/UserTable/index.jsx
+++ src/UserTable/index.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 export default function UserTable() {
-    return <div>User Table</div>
+    return <div className={styles.content}>User Table</div>
 }
