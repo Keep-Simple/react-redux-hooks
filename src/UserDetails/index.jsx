d6bf4e1749ae3940f7417890b46a5d09c67e03f2
KeepSimple <nikita.iasnogorodskyi@startupcraft.io>
2020-12-08 16:25:53 +0200

    User Details Page

--- src/UserDetails/index.jsx
+++ src/UserDetails/index.jsx
@@ -3,10 +3,39 @@ import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import { selectUser } from '../store/selectors'
+import styles from './styles.module.scss'
 
 export default function UserDetails() {
     const { id } = useParams()
-    const user = useSelector((state) => selectUser(state, id))
 
-    return <div>{`User name = ${user.name.first}`}</div>
+    const {
+        gender,
+        email,
+        phone,
+        name: { first, last },
+        dob: { date, age },
+        location: { city, country },
+        picture: { large: avatar },
+    } = useSelector((state) => selectUser(state, id))
+
+    return (
+        <profile className={styles.profile}>
+            <img src={avatar} alt="avatar" />
+            <section>
+                <h3>
+                    Name: {first}, {last}
+                </h3>
+
+                <h3>
+                    Location: {country}, {city}
+                </h3>
+
+                <h3>Email: {email}</h3>
+                <h3>Phone: {phone}</h3>
+                <h3>Birthday: {date.substring(0, 10)}</h3>
+                <h3>Age: {age}</h3>
+                <h3>Gender: {gender}</h3>
+            </section>
+        </profile>
+    )
 }
